3f00a28c867570f4dda35f71a01e33a215402e31
Alex Khomenko <sasvsha@gmail.com>
2021-12-11 11:43:45 +0100

    Change IUserController name

--- src/users/users.controller.interface.ts
+++ src/users/users.controller.interface.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
-export interface IUser {
+export interface IUserController {
 	register: (req: Request, res: Response, next: NextFunction) => void;
 	login: (req: Request, res: Response, next: NextFunction) => void;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,7 +5,7 @@ import { inject, injectable } from 'inversify';
 import { TYPES } from '../types';
 
 import { ILogger } from '../logger/logger.interface';
-import { IUser } from './user.interface';
+import { IUserController } from './users.controller.interface';
 
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
@@ -15,7 +15,7 @@ import { HTTPError } from '../errors/http-error.class';
 import { User } from './user.entity';
 
 @injectable()
-export class UserController extends BaseController implements IUser {
+export class UserController extends BaseController implements IUserController {
 	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
 		super(loggerService);
 

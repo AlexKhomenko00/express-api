2371846d3aa7fae370f93113cd36e6960337656e
Alex Khomenko <sasvsha@gmail.com>
2021-12-24 00:55:37 +0100

    Change return type for get func in config

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -19,7 +19,7 @@ export class ConfigService implements IConfigService {
 		}
 	}
 
-	get<T extends string | number>(key: string): T {
-		return this.config[key] as T;
+	get(key: string): string {
+		return this.config[key];
 	}
 }

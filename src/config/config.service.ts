46313c624b5fbf90b63b12a40e7428436c32f4b1
Alex Khomenko <sasvsha@gmail.com>
2021-12-24 00:41:20 +0100

    Created config service

---
+++ src/config/config.service.ts
@@ -0,0 +1,25 @@
+import { inject, injectable } from 'inversify';
+import { config, DotenvConfigOutput, DotenvParseOutput } from 'dotenv';
+
+import { TYPES } from '../types';
+
+import { IConfigService } from './config.service.interface';
+import { ILogger } from '../logger/logger.interface';
+
+@injectable()
+export class ConfigService implements IConfigService {
+	private config: DotenvParseOutput;
+	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+		const result: DotenvConfigOutput = config();
+		if (result.error) {
+			this.loggerService.error('Error reading .env file or it is missing');
+		} else {
+			this.loggerService.log('[ConfigService] .env config loaded');
+			this.config = result.parsed as DotenvParseOutput;
+		}
+	}
+
+	get<T extends string | number>(key: string): T {
+		return this.config[key] as T;
+	}
+}

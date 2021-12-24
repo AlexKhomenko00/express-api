68dad8c03540419f6794c0ee30ef6128c8ff5be7
Alex Khomenko <sasvsha@gmail.com>
2021-12-24 20:01:33 +0100

    Fix numb response in authMiddleware

--- src/common/auth.middleware.ts
+++ src/common/auth.middleware.ts
@@ -9,19 +9,16 @@ export class AuthMiddleware implements IMiddleware {
 
 	execute(req: Request, res: Response, next: NextFunction): void {
 		if (req.headers.authorization) {
-			const token = req.headers.authorization.split(' ')[1];
-
-			verify(token, this.secret, (err, payload) => {
+			verify(req.headers.authorization.split(' ')[1], this.secret, (err, payload) => {
 				if (err) {
 					next();
 				} else if (payload) {
 					req.user = payload.email;
-
-					next();
-				} else {
 					next();
 				}
 			});
+		} else {
+			next();
 		}
 	}
 }

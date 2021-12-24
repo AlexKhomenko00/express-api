bd994a8319d0df64bf63cb0c09b8a3f858a45e63
Alex Khomenko <sasvsha@gmail.com>
2021-12-24 20:10:29 +0100

    Created auth guard

---
+++ src/common/auth.guard.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { IMiddleware } from './middleware.inteface';
+
+import { HTTPError } from '../errors/http-error.class';
+
+export class AuthGuard implements IMiddleware {
+	execute(req: Request, res: Response, next: NextFunction): void {
+		if (req.user) {
+			return next();
+		}
+
+		res.status(401).send({ error: 'Not authorized' });
+	}
+}
